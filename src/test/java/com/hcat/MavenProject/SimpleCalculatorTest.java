4f6adb0d1d3dcbbbf71320a05d11319fe88f765d
Satya Duggirala <satyaduggirala9969@gmail.com>
2024-02-19 12:29:34 +0530

    Final commit

--- src/test/java/com/hcat/MavenProject/SimpleCalculatorTest.java
+++ src/test/java/com/hcat/MavenProject/SimpleCalculatorTest.java
@@ -39,14 +39,14 @@ public class SimpleCalculatorTest extends TestCase
 		assertEquals(5,result);
 	}
 	
-	/* @Test
+	 @Test
 	    public void testDivisionByZero()
 	 {
 	        SimpleCalculator sc = new SimpleCalculator();
 	        
 	        // This test should throw an IllegalArgumentException
 	        
-	        assertThrows(IllegalArgumentException.class, () -> sc.divide(10, 0));
-	 }*/
+	        assertThrows(IllegalArgumentException.class, () -> sc.divide1(10, 0));
+	 }
 }
 
