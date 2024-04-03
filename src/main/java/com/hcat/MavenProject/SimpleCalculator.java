c8744c88d05b09e931bcba92e8517410d9252136
Satyaduggirala <satyaduggirala9969@gmail.com>
2024-04-03 16:25:31 +0530

    final commit

--- src/main/java/com/hcat/MavenProject/SimpleCalculator.java
+++ src/main/java/com/hcat/MavenProject/SimpleCalculator.java
@@ -1,31 +1,59 @@
 package com.hcat.MavenProject;
 
-public class SimpleCalculator 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleCalculator
 {
-	public int add(int numberA, int numberB)
+	public int add(List<Integer> numberList)
 	{
-		return numberA + numberB;
+		int sum = 0;
+		for (int num : numberList)
+		{
+			sum += num;
+		}
+		return sum;
 	}
 	
-	public int sub(int numberA, int numberB)
+	public int sub(List<Integer> numberList)
 	{
-		return numberA - numberB;
+		int dif = 0;
+		for (int num : numberList)
+		{
+			dif -= num;
+		}
+		return dif;
 	}
 	
-	public int multiply(int numberA, int numberB)
+	public int multiply(List<Integer> numberList)
 	{
-		return numberA * numberB;
+		int product = 1;
+		for (int num : numberList)
+		{
+			product *= num;
+		}
+		return product;
 	}
 
-	public int divide(int numberA, int numberB)
+	public  List<Double> divide(List<Double> numbers, double divisor)
 	{
-		if (numberB == 0)
 		{
-			throw new ArithmeticException("Division by zero");
+			List<Double> result = new ArrayList<>();
+			for (double number : numbers)
+			{
+				if (divisor != 0)
+				{
+					result.add(number / divisor);
+				}
+				else
+				{
+					// Handle division by zero
+					throw new ArithmeticException("Division by zero");
+				}
+			}
+			return result;
 		}
-		return numberA / numberB;
 	}
-	
 }
 
 			
--- src/test/java/com/hcat/MavenProject/SimpleCalculatorTest.java
+++ src/test/java/com/hcat/MavenProject/SimpleCalculatorTest.java
@@ -5,48 +5,55 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import junit.framework.TestCase;
 
-public class SimpleCalculatorTest extends TestCase
-{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleCalculatorTest extends TestCase {
 	@Test
-	public void testAdd()
-	{
-		SimpleCalculator sc = new SimpleCalculator();
-		
-		int result = sc.add(5, 3);
-		assertEquals(8,result);
-	}
-	
-	public void testSub()
-	{
-		SimpleCalculator sc = new SimpleCalculator();
-		
-		int result = sc.sub(5, 3);
-		assertEquals(2,result);
-	}
-	
-	public void testMultiply()
-	{
-		SimpleCalculator sc = new SimpleCalculator();
-		
-		int result = sc.multiply(10, 3);
-		assertEquals(30,result);
-	}
-	
-	public void testDivide()
-	{
-		SimpleCalculator sc = new SimpleCalculator();
-		
-		int result = sc.divide(10, 2);
-		assertEquals(5,result);
-	}
-
-	public void testDivisionByZero()
-	{
+	public void testAdd() {
 		SimpleCalculator sc = new SimpleCalculator();
 
-		// This test should throw an IllegalArgumentException
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(5);
+		numbers.add(10);
+		numbers.add(15);
 
-		assertThrows(ArithmeticException.class, () -> sc.divide(10, 0));
+		int result = sc.add(numbers);
+		assertEquals(30, result);
+	}
+
+	public void testSub() {
+		SimpleCalculator sc = new SimpleCalculator();
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(5);
+		numbers.add(10);
+		numbers.add(15);
+
+		int result = sc.sub(numbers);
+		assertEquals(-30, result);
+	}
+
+	public void testMultiply() {
+		SimpleCalculator sc = new SimpleCalculator();
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(5);
+		numbers.add(10);
+		numbers.add(15);
+
+		int result = sc.multiply(numbers);
+		assertEquals(750, result);
+	}
+
+	public void testDivide() {
+		SimpleCalculator sc = new SimpleCalculator();
+		List<Double> numbers = new ArrayList<>();
+		numbers.add(5.0);
+		numbers.add(10.0);
+		numbers.add(15.0);
+
+		List<Double> result = sc.divide(numbers, 2.0);
+		assertEquals(Arrays.asList(2.5,5.0,7.5), result);
 	}
 }
 
